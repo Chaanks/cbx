163bf5bb9bbda31901ae0414d9fe7cf8e10e75aa
jarod duret <jarod8405@gmail.com>
2019-05-07 23:42:03 +0200

    factory and surface creation

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,69 @@
+#![cfg_attr(
+not(any(feature = "dx12", feature = "metal", feature = "vulkan")),
+allow(unused)
+)]
+
 mod window;
 
-use winit::{EventsLoop, WindowBuilder, Event, WindowEvent};
+use window::WindowState;
+use rendy::{
+    command::{Families, QueueId, RenderPassEncoder},
+    factory::{Config, Factory},
+    graph::{
+        present::PresentNode, render::*, Graph, GraphBuilder, GraphContext, NodeBuffer, NodeImage,
+    },
+    memory::Dynamic,
+    mesh::PosColor,
+    resource::{Buffer, BufferInfo, DescriptorSetLayout, Escape, Handle},
+    shader::{ShaderKind, SourceLanguage, StaticShaderInfo},
+};
+use winit::Window;
 
+#[cfg(feature = "vulkan")]
+type Backend = rendy::vulkan::Backend;
+
+
+#[cfg(any(feature = "dx12", feature = "metal", feature = "vulkan"))]
 fn main() {
     env_logger::Builder::from_default_env()
-        .filter_level(log::LevelFilter::Info)
+        .filter_level(log::LevelFilter::Warn)
         .filter_module("triangle", log::LevelFilter::Trace)
         .init();
 
-    let mut event_loop = EventsLoop::new();
-    let windows = WindowBuilder::new()
-        .with_title("Triangle")
-        .build(&event_loop)
-        .expect("Failed to create window");
-
-    run(&mut event_loop);
+    let mut window = WindowState::default();
+    init(window.window);
 
 
-
-        
 }
 
-fn run(event_loop: &mut EventsLoop) {
-    log::info!("Application running");
-    let mut close  = false;
-    loop {
-        event_loop.poll_events(|event| match event {
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                ..
-            } => close = true,
-            _ => (),
-        });
 
-        if close {
-            log::info!("Close requested");
-            break;
-        }
-    }
+fn init(window: Window) {
+    let config: Config = Default::default();
+
+    // Higher level device interface. Manges memory, resources and queue families.
+    let (mut factory, mut families): (Factory<Backend>, _) = rendy::factory::init(config).expect("Failed to init factory");
+
+    // Rendering target bound to window.
+    let surface = factory.create_surface(window.into());
+
+    // Build graph from nodes and resource.
+    let mut graph_builder = GraphBuilder::<Backend, ()>::new();
+
+    // Create new image owned by graph.
+    let color = graph_builder.create_image(
+        surface.kind(),
+        1,
+        factory.get_surface_format(&surface),
+        Some(gfx_hal::command::ClearValue::Color(
+            [1.0, 1.0, 0.0, 1.0].into(),
+        ))
+    );
+
+
+
+}
+
+#[cfg(not(any(feature = "dx12", feature = "metal", feature = "vulkan")))]
+fn main() {
+    panic!("Specify feature: { dx12, metal, vulkan }");
 }
--- src/window.rs
+++ src/window.rs
@@ -1,22 +1,54 @@
-use winit::{EventsLoop, WindowBuilder, Event, WindowEvent, Window, LogicalSize, CreationError};
+use winit::{EventsLoop, WindowBuilder, Event, WindowEvent, Window, CreationError};
 
-#[Derive(Debug)]
+
+pub const WINDOW_NAME: &str = "Hello Window";
+
+#[derive(Debug)]
 pub struct WindowState {
     pub event_loop: EventsLoop,
     pub window: Window,
 }
 
 impl WindowState {
-    pub fn new<T: into<String>>(title: T, size: LogicalSize) -> Result<Self, CreationError>{
+    pub fn new<T: Into<String>>(title: T, size: (u32, u32)) -> Result<Self, CreationError>{
         let mut event_loop = EventsLoop::new();
-        let windows = WindowBuilder::new()
+        let window = WindowBuilder::new()
             .with_title("Triangle")
-            .with_dimensions(size)
+            .with_dimensions(size.into())
             .build(&event_loop)?;
 
         Ok(Self {
             event_loop,
             window,
         })
+    }
+
+    pub fn run(&mut self) {
+        log::info!("Application running");
+        let mut close  = false;
+        loop {
+            self.event_loop.poll_events(|event| match event {
+                Event::WindowEvent {
+                    event: WindowEvent::CloseRequested,
+                    ..
+                } => close = true,
+                _ => (),
+            });
+
+            if close {
+                log::info!("Close requested");
+                break;
+            }
+        }
+    }
+}
+
+impl Default for WindowState {
+    fn default() -> Self {
+        Self::new(
+            WINDOW_NAME,
+            (800, 600),
+        )
+        .expect("Failed to create window")
     }
 }
